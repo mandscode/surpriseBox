8079d7be1543f008fa8035dc86490f39960f9c04
mandscode.com <mandscode@gmail.com>
2023-02-17 14:36:50 +0530

    rules update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,6 +9,14 @@ const Dashboard = () => {
                 <div className="wave" />
             </div>
             <div className="_activeSection">
+                <ul className="rules">
+                    <h2>Rules:-</h2>
+                    <li>We have a predefined number list in surprise box</li>
+                    <li>We count the number of logins.</li>
+                    <li>
+                        After clicking once on surprise box, your login count will be seen in the predefined number list. If your login count matches then you will become a winner.
+                    </li>
+                </ul>
                 <ActiveSection/>
             </div>
         </>
